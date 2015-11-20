cfb1c13121f6804fb0efed26c65a22d01ec82f04
Teng Li <otec.teng@gmail.com>
2015-11-20 21:46:42 +0800

    add a hello test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 // Database
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest2');
+var db = monk('mong:27017/nodetest2');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- spec/user_spec.js
+++ spec/user_spec.js
@@ -1,6 +1,9 @@
 var request = require('request');
 
 describe("tes api", function() {
+  it("hello", function(done){
+    console.log('this is a hello test');
+  }
   // it("user api_create", function(done){
   //   request
   //     .post('http://localhost:3000/users/')
