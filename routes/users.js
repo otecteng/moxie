3f8c877dd0d2a8344e0f6712ea2066308484ea98
Teng Li <otec.teng@gmail.com>
2015-11-24 17:26:19 +0800

    implement remove

--- routes/users.js
+++ routes/users.js
@@ -49,23 +49,19 @@ router.get('/words/:id', function(req, res, next) {
   }).on('error',function(e){
     res.send(e);
   });  
-}
+});
 
 router.post('/words/delete', function(req, res, next) {
   var db = req.db;
   var collection = db.get('userlist');
   collection.findOne({ mobile:req.body.mobile}).on('success', function (doc) {
-    req.body.id = uuid.v4();
-    if(!doc.words){
-      doc.words =[req.body];
-    }else{
-      doc.words.push(req.body);
-    }
+    var idx = doc.words.findIndex(function(i){i.id==req.body.id});
+    doc.words.splice(idx,1);
     collection.updateById(doc._id.toString(), doc, function(){
       res.send({ msg: 'ok' });
     });
   });
-}
+});
 
 router.post('/words', function(req, res, next) {
   var db = req.db;
--- spec/user_spec.js
+++ spec/user_spec.js
@@ -25,8 +25,8 @@ describe("tes api", function() {
   // });
   // it("user login", function(done){
   //   request
-  //     .post('http://localhost/users/words')
-  //     .form({mobile: '123456',star:1,word:"我是你",date:new Date()})
+  //     .post('http://localhost/users/words/delete')
+  //     .form({mobile: '123456',id:'084211af-718d-4824-b83a-6254d6d1fcd4'})
   //     .on('data', function(data) {
   //       console.log('---'+data);
   //       done();
